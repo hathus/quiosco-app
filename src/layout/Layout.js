46be00896cc43daea3d6f9de7a60bf9910ba25fe
Hathus <trilogyti@gmail.com>
2024-02-10 09:42:50 -0600

    Se agrego styles al menu lateral

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({children, pagina}) {
             </Head>
 
             <div className='md:flex'>
-                <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5'>
+                <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5 flex-col flex items-center'>
                     <Sidebar />
                 </aside>
                 <main className='md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
